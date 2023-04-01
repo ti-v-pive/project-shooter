08b7cfa3534f14f725e6a797495b4080de4c33b4
Татьяна Осадчая <t.osadchaia@perfectplay.io>
2023-04-01 18:44:36 +0200

    Фикс поворота игрока

--- Assets/Scripts/Game/Movement/PlayerRotation.cs
+++ Assets/Scripts/Game/Movement/PlayerRotation.cs
@@ -1,9 +1,16 @@
 ﻿using UnityEngine;
 
 namespace Game.Movement {
-    public class PlayerRotation : MonoBehaviour{
+    public class PlayerRotation : MonoBehaviour {
+        private Transform _transform;
+        
         private void Update() {
-            transform.LookAt(PlayerAimPosition.Instance.transform);
+            _transform ??= transform;
+            _transform.LookAt(PlayerAimPosition.Instance.transform);
+            
+            var rotation = _transform.rotation;
+            rotation.z = 0;
+            _transform.rotation = rotation;
         }
     }
 }
