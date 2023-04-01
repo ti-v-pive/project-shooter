d01a39adc6e03eb643180e46c0f3eff02b7a7022
kruglov-a <a.kruglov@perfectplay.io>
2023-04-01 21:19:24 +0500

    Добавил параметры для аниматора

---
+++ Assets/Scripts/Game/Animation/RigidbodyToAnimatorMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Game.Animation {
+    public class RigidbodyToAnimatorMovement : MonoBehaviour {
+        [SerializeField] private Rigidbody rb;
+        [SerializeField] private Animator animator;
+        [SerializeField] private string horizontalVelocityParameter = "HorizontalVelocity";
+        [SerializeField] private string verticalVelocityParameter = "VerticalVelocity";
+        [SerializeField] private float velocityMultiplier = 1.0f;
+
+        private void Update() {
+            Vector3 localVelocity = transform.InverseTransformDirection(rb.velocity);
+
+            float horizontalVelocity = localVelocity.x * velocityMultiplier;
+            float verticalVelocity = localVelocity.z * velocityMultiplier;
+
+            animator.SetFloat(horizontalVelocityParameter, horizontalVelocity);
+            animator.SetFloat(verticalVelocityParameter, verticalVelocity);
+        }
+    }
+}
