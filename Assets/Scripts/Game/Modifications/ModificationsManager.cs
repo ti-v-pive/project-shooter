9b18f79c742f74b73a4ec676501c801b69f54cd4
Татьяна Осадчая <t.osadchaia@perfectplay.io>
2023-04-02 10:45:56 +0200

    Панели модификаторов щита и двойного урона

--- Assets/Scripts/Game/Modifications/ModificationsManager.cs
+++ Assets/Scripts/Game/Modifications/ModificationsManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace Game {
     public class ModificationsManager {
         private readonly Dictionary<ModificationType, float> _endTimes = new();
-        
+
         public void Clear() {
             _endTimes.Clear();
         }
@@ -15,11 +15,12 @@ namespace Game {
             SetEndTime(type, endTime);
         }
 
+        public bool IsActive(ModificationType type) => TimeLeft(type) > 0;
+        public float TimeLeft(ModificationType type) => GetEndTime(type) - Time.time;
+        
         public bool IsShield => IsActive(ModificationType.Shield);
         public bool IsDoubleDamage => IsActive(ModificationType.DoubleDamage);
-        
-        private bool IsActive(ModificationType type) => Time.time < GetEndTime(type);
-        
+
         private float GetEndTime(ModificationType type) => _endTimes.ContainsKey(type) ? _endTimes[type] : 0;
         private void SetEndTime(ModificationType type, float endTime) {
             if (_endTimes.ContainsKey(type)) {
---
+++ Assets/Scripts/Game/UI/Panels/PlayerModificationPanel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game {
+    public class PlayerModificationPanel : MonoBehaviour {
+        [SerializeField] private ModificationType _type;
+        [SerializeField] private Slider _slider;
+
+        private static ModificationsManager Modifications => Main.Instance.Modifications;
+
+        private bool _isVisible;
+        private float _max;
+        
+        private void Awake() {
+            SetIsVisible(false);
+        }
+
+        private void Update() {
+            var isModificationOn = Modifications.IsActive(_type);
+            if (isModificationOn != _isVisible) {
+                SetIsVisible(isModificationOn);
+
+                if (!_isVisible) {
+                    return;
+                }
+                
+                _max = Modifications.TimeLeft(_type);
+                _slider.value = 1;
+                return;
+            }
+
+            if (!_isVisible) {
+                return;
+            }
+
+            var left = Modifications.TimeLeft(_type);
+            var p = _max > 0 ? Math.Clamp(left / _max, 0f, 1f) : 0f;
+            _slider.value = p;
+        }
+
+        private void SetIsVisible(bool isVisible) {
+            _isVisible = isVisible;
+            _slider.gameObject.SetActive(isVisible);
+        }
+
+    }
+}
