a54a11cd5b177b7ede3caf8d836060702f3dd7b3
Татьяна Осадчая <t.osadchaia@perfectplay.io>
2023-04-02 16:17:48 +0200

    Таймер уровня

---
+++ Assets/Scripts/Game/CountdownTimer.cs
@@ -0,0 +1,42 @@
+﻿using TMPro;
+using UnityEngine;
+using Utils;
+
+namespace Game {
+    public class CountdownTimer : MonoBehaviourSingleton<CountdownTimer> {
+        public float timeRemaining = 60f;
+        public TMP_Text countdownText;
+
+        private bool _isActive;
+
+        protected override void Awake() {
+            base.Awake();
+            _isActive = true;
+        }
+
+        private void Update()
+        {
+            if (!_isActive) {
+                return;
+            }
+            
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                int minutes = Mathf.FloorToInt(timeRemaining / 60f);
+                int seconds = Mathf.FloorToInt(timeRemaining % 60f);
+                countdownText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+            }
+            else {
+                _isActive = false;
+                
+                timeRemaining = 0f;
+                countdownText.text = "00:00";
+                
+                Main.Instance.Lose();
+            }
+        }
+    }
+}
+
+
