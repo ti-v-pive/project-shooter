0b94b18285d60b268445f75cf010ec68a5bf6491
Татьяна Осадчая <t.osadchaia@perfectplay.io>
2023-04-02 10:59:22 +0200

    Панель с монетами

---
+++ Assets/Scripts/Game/UI/Panels/GameItemsPanel.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game {
+    public class GameItemsPanel : MonoBehaviour {
+        [SerializeField] private GameItemType _type;
+        [SerializeField] private Text _countLabel;
+
+        private static GameItemsManager Inventory => Main.Instance.Inventory;
+
+        private void Awake() {
+            Inventory.Get(_type).OnChangeListeners += OnChange;
+            Redraw();
+        }
+
+        private void OnDestroy() {
+            Inventory.Get(_type).OnChangeListeners -= OnChange;
+        }
+
+        private void OnChange() {
+            if (!gameObject) {
+                OnDestroy();
+                return;
+            }
+
+            Redraw();
+        }
+
+        private void Redraw() {
+            var count = Inventory.Get(_type).Count;
+
+            if (_countLabel) {
+                _countLabel.text = count.ToString();
+            }
+        }
+    }
+}
