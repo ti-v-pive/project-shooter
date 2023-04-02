df061a4bf804ac705ca61847a46452b711652870
Татьяна Осадчая <t.osadchaia@perfectplay.io>
2023-04-02 17:20:21 +0200

    Фоновая музыка

---
+++ Assets/Scripts/Game/BackgroundMusic.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game {
+    public class BackgroundMusic : MonoBehaviour {
+        private AudioSource audioSource;
+
+        void Start() {
+            audioSource = GetComponent<AudioSource>();
+            audioSource.Play();
+        }
+    }
+}
