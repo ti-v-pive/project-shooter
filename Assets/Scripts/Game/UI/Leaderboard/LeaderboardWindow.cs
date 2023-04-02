f700a8e7e0cc24b88ed253223545e9987cfaecab
kruglov-a <a.kruglov@perfectplay.io>
2023-04-02 20:44:27 +0500

    Окно лидерборда

--- Assets/Scripts/Game/UI/Leaderboard/LeaderboardWindow.cs
+++ Assets/Scripts/Game/UI/Leaderboard/LeaderboardWindow.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using Utils;
 using UniRx;
+using UnityEngine.Serialization;
 
 namespace Game.UI.Leaderboard {
     public class LeaderboardWindow : MonoBehaviourSingleton<LeaderboardWindow> {
@@ -17,15 +18,13 @@ namespace Game.UI.Leaderboard {
         }
         
         [SerializeField] private TMP_InputField _inputField;
-        [SerializeField] private Button _buttonAccept;
-        [SerializeField] private Button _buttonReject;
+        [SerializeField] private Button _buttonClose;
 
         private Response _response;
 
         protected override void Awake() {
             base.Awake();
-            _buttonAccept.onClick.AddListener(OnAcceptClick);
-            _buttonReject.onClick.AddListener(OnRejectClick);
+            _buttonClose.onClick.AddListener(OnCloseClick);
             HideInternal();
         }
 
@@ -47,31 +46,6 @@ namespace Game.UI.Leaderboard {
         private void ShowInternal() => gameObject.SetActive(true);
         private void HideInternal() => gameObject.SetActive(false);
 
-        private void OnAcceptClick() => _response = Response.Accept;
-        private void OnRejectClick() => _response = Response.Reject;
-
-#if UNITY_EDITOR
-        [ContextMenu("TestUsername")]
-        private void TestNickname() {
-            var result = UsernameManager.TryGetUsername();
-            result.ToObservable().Subscribe(Debug.Log);
-        }
-
-        [ContextMenu("TestAddScore")]
-        private void TestAddScore() {
-            LeaderboardManager.AddScore(1000);
-        }
-        
-        [ContextMenu("TestGetScore")]
-        private void TestGetScore() {
-            LeaderboardManager.GetTopScores(10).ToObservable().Subscribe(ScoreCallback);
-        }
-        
-        private static void ScoreCallback(List<PlayerLeaderboardEntry> list) {
-            foreach (var entry in list) {
-                Debug.Log($"DisplayName: {entry.DisplayName}, Position: {entry.Position}, Score: {entry.StatValue}");
-            }
-        }
-#endif
+        private void OnCloseClick() => _response = Response.Reject;
     }
 }
